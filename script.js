c4bead80a3fe7aacc5e9a8ecd82aed82f3081658
Robbie Ho <69515266+robyhho@users.noreply.github.com>
2022-07-18 14:24:44 +0100

    finished toolkit, and added contact section. Added JS file setters and moveTo function

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 //SETTERS
 
-//GETTERS
+//SELECTORS
+const logo = document.getElementById("logo");
+const navAbout = document.getElementById("navAbout");
+const navWork = document.getElementById("navWork");
+const navToolkit = document.getElementById("navToolkit");
+const navContact = document.getElementById("navContact");
+
+const about = document.getElementById("about");
+const work = document.getElementById("work");
+const toolkit = document.getElementById("toolkit");
+const contact = document.getElementById("contact");
+
+const dot = document.getElementById("dot");
+
+//EVENT LISTENERS
+
+logo.addEventListener("click", moveTo);
+navAbout.addEventListener("click", moveTo);
+navWork.addEventListener("click", moveTo);
+navToolkit.addEventListener("click", moveTo);
+navContact.addEventListener("click", moveTo);
+dot.addEventListener("click", moveTo);
 
 //FUNCTIONS
+
+function moveTo(event) {
+  let target = event.target.id;
+  target = target.replace("nav", "").toLowerCase();
+  console.log(target);
+  const targetElem = document.getElementById(`${target}`);
+  targetElem.scrollIntoView({ behavior: "smooth" });
+}
