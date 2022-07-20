4625877247654be1c8c2923e16847099a2115ef8
Robbie Ho <69515266+robyhho@users.noreply.github.com>
2022-07-20 08:50:11 +0100

    updated script.js showInfo and hideInfo functions

--- script.js
+++ script.js
@@ -14,6 +14,11 @@ const contact = document.getElementById("contact");
 
 const dot = document.getElementById("dot");
 
+const project1 = document.getElementById("project1");
+const project2 = document.getElementById("project2");
+const project3 = document.getElementById("project3");
+const project4 = document.getElementById("project4");
+
 //EVENT LISTENERS
 
 logo.addEventListener("click", moveTo);
@@ -23,6 +28,11 @@ navToolkit.addEventListener("click", moveTo);
 navContact.addEventListener("click", moveTo);
 dot.addEventListener("click", moveTo);
 
+project1.addEventListener("click", showInfo);
+project2.addEventListener("click", showInfo);
+project3.addEventListener("click", showInfo);
+project4.addEventListener("click", showInfo);
+
 //FUNCTIONS
 
 function moveTo(event) {
@@ -30,4 +40,21 @@ function moveTo(event) {
   target = target.replace("nav", "").toLowerCase();
   const targetElem = document.getElementById(`${target}`);
   targetElem.scrollIntoView({ behavior: "smooth" });
+}
+
+function showInfo(event) {
+  event.stopPropagation();
+  let targetDiv = event.target.querySelector(".projectInfo");
+  targetDiv.classList.remove("hide");
+  event.target.removeEventListener("click", showInfo);
+  targetDiv.querySelector(".cross").addEventListener("click", hideInfo);
+}
+
+function hideInfo(event) {
+  event.stopPropagation();
+  let parentElem = event.target.parentNode;
+  parentElem.classList.add("hide");
+  event.target.removeEventListener("click", hideInfo);
+
+  parentElem.parentNode.addEventListener("click", showInfo);
 }
