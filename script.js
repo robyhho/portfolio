f5327e4b6b62cdc9b4ac0d2b4ba034fbd1970fbe
Robbie Ho <69515266+robyhho@users.noreply.github.com>
2022-09-02 14:29:58 +0100

    Added reponsive css for individual screen sizes

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 //SETTERS
-
+let burgerMenuScreenWidth = 800;
+let menuIsOpen = false;
 //SELECTORS
 const loader = document.getElementById("loaderContainer");
-
+const header = document.getElementById("header");
 const logo = document.getElementById("logo");
 const navAbout = document.getElementById("navAbout");
 const navWork = document.getElementById("navWork");
 const navToolkit = document.getElementById("navToolkit");
 const navContact = document.getElementById("navContact");
 const navMenu = document.getElementById("navMenu");
-const menuIcon = document.getElementById("menuIcon");
+const menuIcon = document.getElementById("menuBtn");
 
 const about = document.getElementById("about");
 const work = document.getElementById("work");
@@ -23,11 +24,15 @@ const project2 = document.getElementById("project2");
 const project3 = document.getElementById("project3");
 const project4 = document.getElementById("project4");
 
+const github1 = document.getElementById("github1");
+const github2 = document.getElementById("github2");
+const github3 = document.getElementById("github3");
+const github4 = document.getElementById("github4");
+
 //EVENT LISTENERS
 
 /* Navbar Events */
 menuIcon.addEventListener("click", showMenu);
-document.body.addEventListener("click", hideMenu);
 
 /* MoveTo Events */
 logo.addEventListener("click", moveTo);
@@ -43,7 +48,14 @@ project2.addEventListener("click", showInfo);
 project3.addEventListener("click", showInfo);
 project4.addEventListener("click", showInfo);
 
+github1.addEventListener("click", goToUrl);
+github2.addEventListener("click", goToUrl);
+github3.addEventListener("click", goToUrl);
+github4.addEventListener("click", goToUrl);
+
 //FUNCTIONS
+
+/* Sets width of body to  */
 
 /* Loader Removal function  */
 // TODO Re-enable Loader
@@ -56,15 +68,35 @@ project4.addEventListener("click", showInfo);
 /* MoveTo Function
   Moves window to id of target elem.
 */
+
 function moveTo(event) {
   let target = event.target.id;
   if (target === "dot" || target === "arrowUp") {
-    logo.scrollIntoView({ behavior: "smooth" });
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
     return;
   }
+
   target = target.replace("nav", "").toLowerCase();
+
+  if (target == "logo") {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+    return;
+  }
   const targetElem = document.getElementById(`${target}`);
   targetElem.scrollIntoView({ behavior: "smooth" });
+  menuIcon.classList.remove("open");
+  menuIsOpen = false;
+  if (window.screen.width <= burgerMenuScreenWidth) {
+    navMenu.style.display = "none";
+  }
 }
 
 /* showInfo Func
@@ -72,7 +104,7 @@ function moveTo(event) {
   sets event listener on x button
 */
 function showInfo(event) {
-  event.stopPropagation();
+  event.stopPropagation;
   let targetDiv = event.target.querySelector(".projectInfo");
   targetDiv.classList.remove("hide");
   event.target.removeEventListener("click", showInfo);
@@ -97,9 +129,13 @@ function hideInfo(event) {
 */
 function showMenu(event) {
   event.stopPropagation;
-  if (navMenu.style.display === "none") {
+  if (!menuIsOpen) {
+    menuIcon.classList.add("open");
+    menuIsOpen = true;
     navMenu.style.display = "flex";
   } else {
+    menuIcon.classList.remove("open");
+    menuIsOpen = false;
     navMenu.style.display = "none";
   }
 }
@@ -109,9 +145,35 @@ function showMenu(event) {
   when clicked item
   is not menuIcon
 */
-function hideMenu(event) {
-  event.stopPropagation;
-  if (event.target.id !== "menuIcon") {
-    navMenu.style.display = "none";
+// function hideMenu(event) {
+//   event.stopPropagation;
+//   if (event.target.id !== "menuIcon" || event.target.id !== "menuBtn") {
+//     navMenu.style.display = "none";
+//   }
+// }
+
+function goToUrl(event) {
+  switch (event.target.id) {
+    case "github1":
+      window.open(
+        "https://github.com/robyhho/showCaseProjects/tree/master/rock-paper-scissors",
+        "_blank"
+      );
+      break;
+    case "github2":
+      window.open("https://github.com/robyhho/bbakery", "_blank");
+      break;
+    case "github3":
+      window.open(
+        "https://github.com/robyhho/showCaseProjects/tree/master/todo-app-main",
+        "_blank"
+      );
+      break;
+    case "github4":
+      window.open(
+        "https://github.com/robyhho/showCaseProjects/tree/master/tip-calculator",
+        "_blank"
+      );
+      break;
   }
 }
